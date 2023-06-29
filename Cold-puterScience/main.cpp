86741f0c8fd58eef926cc959d235c6217a034408
Bui Phuong Nam <88639992+arsdorintbp2003@users.noreply.github.com>
2023-06-29 22:24:00 +0700

    Create main.cpp

---
+++ Cold-puterScience/main.cpp
@@ -0,0 +1,12 @@
+//Problem Link: https://open.kattis.com/problems/cold
+#include <iostream>
+using namespace std;
+int main() {
+    int N; cin >> N;
+    int count = 0;
+    for (int i = 0; i < N; i++) {
+        int num; cin >> num;
+        if (num < 0) count++;
+    }
+    cout << count;
+}
