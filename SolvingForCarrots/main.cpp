65999edf6042a8e1e321c291464d719f7b1231a5
Bui Phuong Nam <88639992+arsdorintbp2003@users.noreply.github.com>
2023-07-11 22:23:21 +0700

    Create main.cpp

---
+++ SolvingForCarrots/main.cpp
@@ -0,0 +1,10 @@
+//Problem Link: https://open.kattis.com/problems/carrots
+#include <iostream>
+using namespace std;
+int main() {
+    int N, K; cin >> N >> K;
+    for (int i = 0; i < N; i++) {
+        string k; cin >> k;
+    }
+    cout << K;
+}
