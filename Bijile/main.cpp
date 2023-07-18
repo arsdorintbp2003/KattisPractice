86d1a939884cbe9f6ceb56c7a5de99586c7a3e8c
Bui Phuong Nam <88639992+arsdorintbp2003@users.noreply.github.com>
2023-07-18 22:44:32 +0700

    Create main.cpp

---
+++ Bijile/main.cpp
@@ -0,0 +1,8 @@
+//Problem Link: https://open.kattis.com/problems/bijele
+#include <iostream>
+using namespace std;
+int main() {
+    int k, q, r, b, kn, p; cin >> k >> q >> r >> b >> kn >> p;
+    cout << 1 - k << " " << 1 - q << " " << 2 - r << " " << 2 - b 
+    << " " << 2 - kn << " " << 8 - p;
+}
