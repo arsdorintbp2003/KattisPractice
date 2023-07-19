77f74a82c50c1f82f4685c70984e44b833701b68
Bui Phuong Nam <88639992+arsdorintbp2003@users.noreply.github.com>
2023-07-19 22:48:53 +0700

    Create main.cpp

---
+++ StuckInATimeLoop/main.cpp
@@ -0,0 +1,7 @@
+//Problem Link: https://open.kattis.com/problems/timeloop
+#include <iostream>
+using namespace std;
+int main() {
+    int num; cin >> num;
+    for (int i = 0; i < num; i++) cout << i + 1 << " Abracadabra" << endl;
+}
